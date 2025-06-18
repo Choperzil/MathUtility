a3153962ddc33a374cb83866341b7688786b1e7f
Choperzil <chopperzil2005@gmail.com>
2025-06-18 11:13:26 +0700

    Fix value to green 4ever

--- TestExponentialFunction/CalculatorTest.cs
+++ TestExponentialFunction/CalculatorTest.cs
@@ -6,20 +6,20 @@ namespace TestExponentialFunction
 	public class CalculatorTest
 	{
 		[Theory]
-		[InlineData(2, 2, 5)]
+		[InlineData(2, 2, 4)]
 		[InlineData(3, 5, 243)]
 		[InlineData(3.4, 3, 39.304)]
 		[InlineData(5, 0, 1)]
 		[InlineData(10, 1, 10)]
 		[InlineData(-2, 3, -8)]
-		public void testExponentialReturnsRight(double a, int b, double expected)
+		public void TestExponentialReturnsRight(double a, int b, double expected)
 		{
 			double result = Exponential.power(a, b);
 			Assert.Equal(result, expected, 5);
 		}
 
 		[Fact]
-		public void testExponentialReturnsException()
+		public void TestExponentialReturnsException()
 		{
 			var ex = Assert.Throws<ArgumentException>(() => Exponential.power(0, 0));
 			Assert.Equal("a or b must bigger than 0", ex.Message);
@@ -28,7 +28,7 @@ namespace TestExponentialFunction
 		[Theory]
 		[InlineData(2, 1001)]
 		[InlineData(2, -1001)]
-		public void testExponentialReturnsException1(double a, int b)
+		public void TestExponentialReturnsException1(double a, int b)
 		{
 			var ex = Assert.Throws<ArgumentException>(() => Exponential.power(a, b));
 			Assert.Equal("Value b is out of range", ex.Message);
